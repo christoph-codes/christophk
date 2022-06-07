341865c20f0c5e7788a2b64e7eba95d55d97a107
Christopher Jones <tkcwebdev@gmail.com>
2022-06-07 01:43:54 -0700

    master: Update lets chat to use new section and updated down arrow and new button

--- app3/src/components/LetsChat/LetsChat.js
+++ app3/src/components/LetsChat/LetsChat.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Section from '../UI/Section/Section';
 import chat from '../assets/chat.svg';
 import './LetsChat.scss';
+import Button from '../Button/Button';
 
 export default function LetsChat(props) {
 	return (
 		<div className='LetsChat'>
-			<Section id='lets-chat' bgColor="white">
+			<Section id='lets-chat' variant='secondary'>
 				<img
 					src={chat}
 					alt='Christopher Jones would like to chat with you about your project'
@@ -17,11 +18,9 @@ export default function LetsChat(props) {
 					your project whether you need it branded or built from scratch. Let’s
 					chat and get the ball rolling on creating something awesome together!
 				</p>
-				<p>
-					<a className='btn' href='mailto:tkcwebdev@gmail.com' rel="noopener noreferrer" target='_blank'>
-						Let's Chat
-					</a>
-				</p>
+				<Button variant='primary' href='mailto:tkcwebdev@gmail.com' rel="noopener noreferrer" target='_blank'>
+					Let's Chat
+				</Button>
 			</Section>
 		</div>
 	);
