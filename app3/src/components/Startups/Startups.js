dd5282df5429710102256e9dacdeb78ddf4d4ce8
Christopher Jones <tkcwebdev@gmail.com>
2022-06-07 01:43:08 -0700

    master: Update startup list to be more current

--- app3/src/components/Startups/Startups.js
+++ app3/src/components/Startups/Startups.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Section from '../UI/Section/Section';
-import { AiOutlineArrowDown } from 'react-icons/ai';
+import { BsChevronDoubleDown } from 'react-icons/bs';
 import lightbulb from '../assets/lightbulb.svg';
 import './Startups.scss';
 
@@ -34,30 +34,42 @@ export default function Startups(props) {
 			link: 'https://getgrubber.com',
 		},
 		{
-			name: 'ShadeShip',
-			emoji: '🕶',
-			desc:
-				'Shade Ship is the premier distributor of quality vintage and retro sunglasses picked to compliment your style.',
-			link: 'https://facebook.com/shadeship',
+			name: 'Bulb',
+			emoji: '💡',
+			desc: 'A community of ideas by founders and entrepreneurs. Partnering with the infamous Eric Feuerstein @fireprone',
+			link: 'https://github.com/christoph-codes/bulb',
 		},
 		{
-			name: 'mySoloGym',
-			emoji: '🏋🏽‍♂️',
-			desc:
-				'MySoloGym provides a private workout experience to fitness enthusiasts who focus on the quality of workout without the distractions.',
-			link: 'https://facebook.com/mysologym',
+			name: 'Reverse Recipe',
+			emoji: '🍗',
+			desc: 'Use your ingredients to find the perfect recipe.',
+			link: 'https://github.com/DebitTwo/reverseRecipe',
 		},
-		{
-			name: 'MommyFits',
-			emoji: '👗',
-			desc:
-				'MommyFits was a mom inspired apparel line and lifestyle brand that was run by my wife with the expectation to inspire moms around the world to be the best “Fit mom” they could be.',
-			link: 'https://facebook.com/mommyfits',
-		},
+		// {
+		// 	name: 'ShadeShip',
+		// 	emoji: '🕶',
+		// 	desc:
+		// 		'Shade Ship is the premier distributor of quality vintage and retro sunglasses picked to compliment your style.',
+		// 	link: 'https://facebook.com/shadeship',
+		// },
+		// {
+		// 	name: 'mySoloGym',
+		// 	emoji: '🏋🏽‍♂️',
+		// 	desc:
+		// 		'MySoloGym provides a private workout experience to fitness enthusiasts who focus on the quality of workout without the distractions.',
+		// 	link: 'https://facebook.com/mysologym',
+		// },
+		// {
+		// 	name: 'MommyFits',
+		// 	emoji: '👗',
+		// 	desc:
+		// 		'MommyFits was a mom inspired apparel line and lifestyle brand that was run by my wife with the expectation to inspire moms around the world to be the best “Fit mom” they could be.',
+		// 	link: 'https://facebook.com/mommyfits',
+		// },
 	];
 	return (
 		<div className='Startups'>
-			<Section id='micro-startups' bgColor={`white`}>
+			<Section id='micro-startups' variant="tertiary">
 				<img
 					src={lightbulb}
 					alt='Christopher Jones comes up with ideas all the time, and here are his ideas has brought to life'
@@ -77,7 +89,7 @@ export default function Startups(props) {
 					);
 				})}
 				<a href='#milestones' uk-scroll='true'>
-					<AiOutlineArrowDown size='3em' />
+					<BsChevronDoubleDown size='3em' color="" />
 				</a>
 			</Section>
 		</div>
