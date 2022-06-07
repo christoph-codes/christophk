bbc12c43544b82c0a75771832d54039584bb5de2
Christopher Jones <tkcwebdev@gmail.com>
2022-06-07 01:44:46 -0700

    master: Use new memoji and update colors in header with new hamburger

--- app3/src/components/Header/Header.js
+++ app3/src/components/Header/Header.js
@@ -2,9 +2,9 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import "./Header.scss";
 import { FaTimes } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
 import UIkit from 'uikit';
-import logo from '../assets/christophk_logo.svg';
-import hamburger from '../assets/hamburger.svg';
+import logo from '../assets/memoji_blank.svg';
 
 export default function Header(props) {
 
@@ -35,7 +35,7 @@ export default function Header(props) {
     },
   ];
   return (
-    <div className='Header'>
+    <header className='Header'>
       <div className="uk-container">
         <div className="uk-grid">
           <div className="uk-width-1-2">
@@ -51,14 +51,16 @@ export default function Header(props) {
           <div className="uk-width-1-2">
             <div className='nav'>
               <a href="#navigation" uk-toggle="target: #navigation">
-                <img
-                  src={hamburger}
-                  alt="Christopher Kirk Jones logo icon"
-                />
+              {/* <img src='#' alt="hello" /> */}
+                <button>
+                  <GiHamburgerMenu color="var(--secon)" size="3rem" />
+                </button>
               </a>
               <div id="navigation" uk-offcanvas="flip: true; overlay: true;">
                 <div className="uk-offcanvas-bar">
+                  <button className='uk-offcanvas-close' type="button" >
                     <FaTimes onClick={closeNav} size="1.5em" />
+                  </button>
                   <div className='mobile_nav_content'>
                     {navItems.map((item) => {
                       return (
@@ -76,6 +78,6 @@ export default function Header(props) {
           </div>
         </div>
       </div>
-    </div>
+    </header>
   );
 }
