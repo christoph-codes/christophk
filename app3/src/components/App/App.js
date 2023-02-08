9998c12e5a1db1a0aa5a4cdc8f1c979bcb7028c2
Christopher Jones <tkcwebdev@gmail.com>
2023-02-08 10:20:07 -0800

    27: Reorder sections for priority

--- app3/src/components/App/App.js
+++ app3/src/components/App/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Footer from '../Footer/Footer';
-import Company from '../Company/Company';
-import Intro from '../Intro/Intro';
-import Startups from '../Startups/Startups';
-import './App.scss';
-import LetsChat from '../LetsChat/LetsChat';
-import Milestones from '../Milestones/Milestones';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Footer from "../Footer/Footer";
+import Company from "../Company/Company";
+import Intro from "../Intro/Intro";
+import Startups from "../Startups/Startups";
+import "./App.scss";
+import LetsChat from "../LetsChat/LetsChat";
+import Milestones from "../Milestones/Milestones";
 
 function App() {
 	return (
 		<Router>
-			<div className='App'>
+			<div className="App">
 				<Intro />
-				<Company />
-				<Startups />
 				<Milestones />
+				<Startups />
+				<Company />
 				<LetsChat />
 				<Footer />
 			</div>
